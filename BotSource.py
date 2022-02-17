0619461d3c3f95e27af3c4fe0e100537baef460c
Aaron IOL <aaronlimbat@gmail.com>
2022-02-17 16:52:46 -0500

    add timing to prevent day trading

--- BotSource.py
+++ BotSource.py
@@ -1,4 +1,14 @@
-import alpaca_trade_api as tradeapi
+#import alpaca_trade_api as tradeapi
+import datetime
+import time
+lastGMETrade = datetime.datetime.now()
+time.sleep(100)
+#When trading, confirm it is not a day trade 
+if lastGMETrade.day == datetime.datetime.now().day:
+    print("Day Trade - CANCELLED" + "\n" + "Last traded" + str(datetime.datetime.now() - lastGMETrade))
+
+"""
+
 privateKey = ""
 publicKey = ""
 baseURL = "https://paper-api.alpaca.markets/"
@@ -7,3 +17,4 @@ api = tradeapi.REST(publicKey, privateKey, baseURL)
 bars = api.get_bars("GME", 'minute', 5)
 for bar in bars:
     print(bar)
+    """
